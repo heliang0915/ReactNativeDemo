ba0fe0b5e458d14b2c5be9bedbf2770eeaed1288
heiang <598481337@qq.com>
2017-08-16 08:33:17 +0800

    优化IReader

--- app/components/IReader.js
+++ app/components/IReader.js
@@ -4,84 +4,129 @@
  * Date: 2017/7/21.
  */
 import React, {Component} from 'react';
-import {Text, ScrollView,StatusBar, StyleSheet,FlatList, View, Image} from 'react-native';
+import {Text, ScrollView, StatusBar, StyleSheet, FlatList, View, Image} from 'react-native';
 import {deviceHeight, deviceWidth, pxToDp} from '../util/pxToDp';
 import {_formatChapter, _contentFormat} from '../util/FromateUtil';
 import commonStyle from '../commonstyle/common';
 import {get} from '../util/FetchUtil';
-import {toastShort} from '../util/ToashUtil'
-
-console.log(_formatChapter);
-
+import Toash from '../util/ToashUtil'
 import {BOOK_INFO_URL, BOOK_CHAPTERS_URL, BOOK_CHAPTERS_CONTENT_URL} from '../api/ApIURL';
 
 class IReader extends Component {
     constructor(props) {
         super(props);
-        this.bookId='58809e839a05e10e3625f046';
+        this.bookId = '58809e839a05e10e3625f046';
         this.chapterPageSize = 0;// 每个章节最终分为几页
-        this.currentChapter=0;
+        this.currentChapter = 0;
     }
 
     state = {
         characterContents: [],
-        loading:false
+        loading: false,
+        chapterLinks: []
+
     }
+
+
+    //获取书的所有章节
+    getAllCharacter() {
+        let BOOKCHAPTERSURL = BOOK_CHAPTERS_URL(this.bookId);
+        get(BOOKCHAPTERSURL).then(({mixToc}) => {
+            let {chapters} = mixToc;
+            this.setState({
+                chapterLinks: chapters
+            }, () => {
+                // this.getCharacter(this.bookId, 0);
+                // this.loadData();
+                chapters.forEach((item,index)=>{
+                    this.loadData(index);
+                })
+
+            })
+        })
+    }
+
+    loadData(num) {
+        if (num == undefined) {
+            this.setState({
+                loading: true
+            });
+            num = 0;
+        }
+        this.getCharacter(this.bookId, num);
+        // setTimeout(()=>{
+        //     num++
+        //     console.log(num);
+        //     console.log(this.state.chapterLinks.length);
+        //     if(num<this.state.chapterLinks.length){
+        //         this.loadData(num)
+        //     }else{
+        //         this.setState({
+        //             loading:false
+        //         });
+        //     }
+        // },2000)
+    }
+
+
     //获取指定书籍的指定章节
     getCharacter(bookId, num) {
         // let counter=0;
-        let list =[] ;
+        let list = [];
         let reg = /第(\d+)章/;
         let inx = 0;
-        let counter=0;
-        this.setState({
-            loading:true
-        });
+        let counter = 0;
+
         // 获取所有章节
         // console.time('耗时')
-        this.currentChapter=num;
-        let BOOKCHAPTERSURL = BOOK_CHAPTERS_URL(bookId);
-        get(BOOKCHAPTERSURL).then(({mixToc}) => {
-            let {chapters} = mixToc;
-
-            chapters.forEach((con)=>{
-                let {link, title} = con;//[num];
-                let BOOKCHAPTERSCONTENTURL = BOOK_CHAPTERS_CONTENT_URL(link);
-                get(BOOKCHAPTERSCONTENTURL).then(({chapter}) => {
-                    title.replace(reg, function () {
-                        inx = RegExp.$1;
-                    })
-                    let {body} = chapter;
-                    var ary = _formatChapter(body);
-                    this.chapterPageSize=this.state.characterContents.concat(ary).length;
-                    ary.forEach((chunk, index) => {
-                        list.push({
-                            key: parseInt(inx) * 1000 + parseInt(index),
-                            characterName: title,
-                            characterContent: [chunk]
-                        });
-                    })
-                    counter++;
-                    if(counter==chapters.length){
-                        list= this.state.characterContents.concat(list);
-                        list.sort((a, b) => {
-                            return a.key - b.key;
-                        })
-                        this.setState({
-                            characterContents:list,
-                            loading:false
-                        })
-                    }
-
-
-                })
+        this.currentChapter = num;
+        // let BOOKCHAPTERSURL = BOOK_CHAPTERS_URL(bookId);
+        // get(BOOKCHAPTERSURL).then(({mixToc}) => {
+        let chapters = this.state.chapterLinks;
+        // chapters.forEach((con)=>{
+        let {link, title} = chapters[num];
+        console.log("num>>>" + num);
+        console.log("link>>>" + link)
+        let BOOKCHAPTERSCONTENTURL = BOOK_CHAPTERS_CONTENT_URL(link);
+        get(BOOKCHAPTERSCONTENTURL).then(({chapter}) => {
+            title.replace(reg, function () {
+                inx = RegExp.$1;
             })
+            let {body} = chapter;
+            var ary = _formatChapter(body);
+            this.chapterPageSize = this.state.characterContents.concat(ary).length;
+            ary.forEach((chunk, index) => {
+                list.push({
+                    key: parseInt(num) * 1000 + parseInt(index),
+                    characterName: title,
+                    characterContent: [chunk]
+                });
+            })
+            // counter++;
+            // if(counter==chapters.length){
+            list = this.state.characterContents.concat(list);
+            list.sort((a, b) => {
+                return a.key - b.key;
+            })
+
+            // cb(list);
+
+            this.setState({
+                characterContents:list,
+                loading:false
+            })
+            // }
+
+
+            // })
+            // })
 
         })
     }
 
     componentDidMount() {
-        this.getCharacter(this.bookId, 0);
+        // this.getCharacter(this.bookId, 0);
+        this.getAllCharacter();
     }
 
     emptyComponent() {
@@ -123,21 +168,20 @@ class IReader extends Component {
         )
     }
 
-    _scrollEnd(env){
-        let {x}=env.nativeEvent.contentOffset;
-        let maxWidth=(this.chapterPageSize-1)*deviceWidth-300;
-        //加载下一个章节信息
-        if(x>maxWidth){
-            // alert("加载下一章节");
-            this.currentChapter+=1;
-            this.getCharacter(this.bookId,this.currentChapter);
-        }
-
-        if(x==0){
+    _scrollEnd(env) {
+        let {x} = env.nativeEvent.contentOffset;
+        // let maxWidth=(this.chapterPageSize-1)*deviceWidth-300;
+        // //加载下一个章节信息
+        // if(x>maxWidth){
+        //     // alert("加载下一章节");
+        //     this.currentChapter+=1;
+        //     this.getCharacter(this.bookId,this.currentChapter);
+        // }
+        // console.log(x);
+        if (x == 0) {
             // ToastAndroid.show('已经是第一页',ToastAndroid.SHORT)
-            toastShort('已经是第一页');
+            Toash.toastShort('已经是第一页');
         }
-        console.log(x);
     }
 
 
@@ -147,11 +191,11 @@ class IReader extends Component {
     // }
 
 
-    renderLoading(){
+    renderLoading() {
         return (
             <View style={[readerStyle.loadingView]}>
                 <View style={readerStyle.loadingInnerView}>
-                    <Image  style={{height: 40, width: 40, marginTop: 10, marginBottom: 10}}
+                    <Image style={{height: 40, width: 40, marginTop: 10, marginBottom: 10}}
                            source={require('../assets/loadding.gif')}/>
                     <Text style={readerStyle.loadingTxt}>加载中...</Text>
                 </View>
@@ -160,38 +204,39 @@ class IReader extends Component {
     }
 
     render() {
+        console.log('render....');
         // alert(this.state.loading==true);
         return (
 
-                <Image source={require('../assets/read_bg.jpg')} style={readerStyle.readerBg}>
-                    {
-                        (this.state.loading==true) ?this.renderLoading():null
-                    }
-                    {/*<StatusBar*/}
-                        {/*backgroundColor="blue"*/}
-                        {/*barStyle="light-content"*/}
-                    {/*/>*/}
-                    <ScrollView
+            <Image source={require('../assets/read_bg.jpg')} style={readerStyle.readerBg}>
+                {
+                    (this.state.loading == true) ? this.renderLoading() : null
+                }
+                <StatusBar
+                backgroundColor="blue"
+                barStyle="light-content"
+                />
+                <ScrollView
+                    horizontal={true}
+                    pagingEnabled={true}
+                    showsHorizontalScrollIndicator={true}
+                    scrollEventThrottle={800}
+                    onMomentumScrollEnd={this._scrollEnd.bind(this)}
+                >
+                    <FlatList
+                        extraData={this.state.characterContents}
+                        numColumns={1}
                         horizontal={true}
-                        pagingEnabled={true}
-                        showsHorizontalScrollIndicator={true}
-                        scrollEventThrottle={800}
-                        onMomentumScrollEnd={this._scrollEnd.bind(this)}
-                    >
-                        <FlatList
-                            extraData={this.state.characterContents}
-                            numColumns={1}
-                            horizontal={true}
-                            data={this.state.characterContents}
-                            renderItem={this.renderItem}
-                            // ListHeaderComponent={this.renderHeader()}
-                            // ListFooterComponent={this.renderFooter()}
-                            ListEmptyComponent={this.emptyComponent()}
-                            initialNumToRender={9}
-                            // contentContainerStyle={readerStyle.readerContent}
-                        />
-                    </ScrollView>
-                </Image>
+                        data={this.state.characterContents}
+                        renderItem={this.renderItem}
+                        // ListHeaderComponent={this.renderHeader()}
+                        // ListFooterComponent={this.renderFooter()}
+                        ListEmptyComponent={this.emptyComponent()}
+                        initialNumToRender={9}
+                        // contentContainerStyle={readerStyle.readerContent}
+                    />
+                </ScrollView>
+            </Image>
         )
     }
 }
@@ -217,7 +262,7 @@ const readerStyle = StyleSheet.create({
         width: deviceWidth,
         height: deviceHeight,
         position: 'absolute',
-        zIndex:3
+        zIndex: 3
     },
     loadingInnerView: {
         height: pxToDp(250),
@@ -226,7 +271,7 @@ const readerStyle = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#000',
-        opacity:1
+        opacity: 1
     },
     loadingAnimate: {
         borderWidth: 5,
--- app/util/ToashUtil.js
+++ app/util/ToashUtil.js
@@ -3,48 +3,37 @@
  * User: heliang
  * Date: 2017/8/15.
  */
-import Toast from 'react-native-root-toast';
-import {ToastAndroid} from 'react-native';
-
-let toast;
-/**
- * 冒一个时间比较短的Toast
- * @param content
- */
-export const toastShort = (content) => {
-
-    ToastAndroid.show(content,ToastAndroid.SHORT)
-    // if (toast !== undefined) {
-    //     Toast.hide(toast);
-    // }
-    // toast = Toast.show(content.toString(), {
-    //     duration: Toast.durations.SHORT,
-    //     position: Toast.positions.CENTER,
-    //     shadow: true,
-    //     animation: true,
-    //     hideOnPress: true,
-    //     delay: 0,
-    //     onShow: () => {
-    //         alert('show');
-    //     }
-    // });
-};
-
-/**
- * 冒一个时间比较久的Toast
- * @param content
- */
-export const toastLong = (content) => {
-    // if (toast !== undefined) {
-    //     Toast.hide(toast);
-    // }
-    // toast = Toast.show(content.toString(), {
-    //     duration: Toast.durations.LONG,
-    //     position: Toast.positions.CENTER,
-    //     shadow: true,
-    //     animation: true,
-    //     hideOnPress: true,
-    //     delay: 0
-    // });
-    ToastAndroid.show(content,ToastAndroid.LONG)
-};
+import Toast from 'react-native-root-toast'
+var toast = null
+module.exports = {
+    toastShort: (message) => {
+        this.toast && this.toast.destroy()
+        this.toast = Toast.show(message, {
+            duration: Toast.durations.SHORT,
+            position: Toast.positions.BOTTOM,
+            shadow: false,
+            animation: true,
+            hideOnPress: false,
+            delay: 0,
+            onHidden: () => {
+                this.toast.destroy()
+                this.toast = null
+            }
+        })
+    },
+    toastLong: (message) => {
+        this.toast && this.toast.destroy()
+        this.toast = Toast.show(message, {
+            duration: Toast.durations.Long,
+            position: Toast.positions.BOTTOM,
+            shadow: false,
+            animation: true,
+            hideOnPress: false,
+            delay: 0,
+            onHidden: () => {
+                this.toast.destroy()
+                this.toast = null
+            }
+        })
+    }
+}
